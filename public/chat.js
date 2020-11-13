d6c92f58dc5bb46c627716778a61c14795b88d7d
Francois Poguet <francoispoguet@gmail.com>
2020-11-13 14:42:58 +0100

    try pre-commit

--- public/chat.js
+++ public/chat.js
@@ -1,297 +1,323 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function (_e) {
+  // Socket connection
+  let sock = io.connect();
+  let pseudo = null;
+  let list = [];
 
-    // Socket connection
-    let sock = io.connect();
-    let pseudo = null;
-    let list = [];
+  // DOM Elements
+  let content = document.getElementById("content");
+  let radLogScreen = document.getElementById("radio1");
+  let pseudoInput = document.getElementById("pseudo");
+  let radContentScreen = document.getElementById("radio2");
+  let asideClients = document.querySelector("#content aside");
+  let btnConnect = document.getElementById("btnConnecter");
+  let btnLogout = document.getElementById("btnQuitter");
+  let chatMain = document.querySelector("#content main");
+  let messageInput = document.getElementById("monMessage");
+  let btnSend = document.getElementById("btnEnvoyer");
 
-    // DOM Elements
-    let content = document.getElementById("content");
-    let radLogScreen = document.getElementById("radio1");
-    let pseudoInput = document.getElementById("pseudo")
-    let radContentScreen = document.getElementById("radio2")
-    let asideClients = document.querySelector("#content aside");
-    let btnConnect = document.getElementById("btnConnecter");
-    let btnLogout = document.getElementById("btnQuitter");
-    let chatMain = document.querySelector("#content main");
-    let messageInput = document.getElementById("monMessage");
-    let btnSend = document.getElementById("btnEnvoyer");
+  let history = {
+    commands: [],
+    index: -1,
+    prev: () => {
+      if (history.index < history.commands.length - 1) {
+        history.index++;
+        return history.commands[history.index];
+      }
+    },
+    next: () => {
+      if (history.index !== -1) {
+        history.index--;
+        return history.commands[history.index];
+      }
+    },
+  };
 
-    let history = {
-        commands : [],
-        index : -1,
-        prev : () => {
-            if(history.index < history.commands.length - 1) {
-                history.index++;
-                return history.commands[history.index]
-            }
-        },
-        next : () => {
-            if(history.index !== -1){
-                history.index--;
-                return history.commands[history.index]
-            }
-        }
+  let autoCompleteChifoumi = {
+    elements: ["rock", "paper", "scissors", "spock", "lizard"],
+    index: 0,
+    next: () => {
+      let res = autoCompleteChifoumi.elements[autoCompleteChifoumi.index];
+      autoCompleteChifoumi.index =
+        (autoCompleteChifoumi.index + 1) % autoCompleteChifoumi.elements.length;
+      return res;
+    },
+  };
 
+  // *****    LISTENERS     *****
+
+  asideClients.ondblclick = (e) => {
+    if (e.target.tagName === "P" && e.target.textContent !== pseudo) {
+      messageInput.value = `/chifoumi @${e.target.textContent} :`;
+      messageInput.focus();
     }
+  };
 
-    let autoCompleteChifoumi = {
-        elements : ['rock','paper','scissors','spock','lizard'],
-        index : 0,
-        next : () => {
-            let res = autoCompleteChifoumi.elements[autoCompleteChifoumi.index];
-            autoCompleteChifoumi.index = (autoCompleteChifoumi.index + 1) % autoCompleteChifoumi.elements.length;
-            return res;
-        }
+  // Display login screen
+  radLogScreen.checked = true;
+  pseudoInput.focus();
 
-    };
-
-    // *****    LISTENERS     *****
-
-    asideClients.ondblclick = e => {
-        if (e.target.tagName === 'P' && e.target.textContent !== pseudo) {
-            messageInput.value = `/chifoumi @${e.target.textContent} :`;
-            messageInput.focus();
-        }
+  btnConnect.onclick = () => {
+    pseudo = pseudoInput.value.trim();
+    if (pseudo.length === 0) {
+      alert("Votre pseudo ne doit pas être vide ! ");
+      return;
     }
+    if (!/^\S+$/.test(pseudo)) {
+      alert("Votre pseudo ne doit pas contenir d'espaces ! ");
+      return;
+    }
+    sock.emit("login", pseudo);
+  };
 
-    // Display login screen
+  pseudoInput.onkeydown = (e) => {
+    if (e.key === "Enter") {
+      btnConnect.click();
+    }
+  };
+
+  btnLogout.onclick = () => {
+    sock.emit("logout");
     radLogScreen.checked = true;
-    pseudoInput.focus();
+  };
 
-
-    btnConnect.onclick = () => {
-        pseudo = pseudoInput.value.trim()
-        if(pseudo.length === 0){
-            alert("Votre pseudo ne doit pas être vide ! ");
-            return;
-        }
-        if(!/^\S+$/.test(pseudo)){
-            alert("Votre pseudo ne doit pas contenir d'espaces ! ");
-            return;
-        }
-        sock.emit("login", pseudo)
+  messageInput.addEventListener("input", function (e) {
+    if (messageInput.value.startsWith("/")) {
+      AutoCompleteSmileys.setPause(true);
+    } else {
+      AutoCompleteSmileys.setPause(false);
     }
+  });
 
-    pseudoInput.onkeydown = (e) => {
-        if(e.key === 'Enter'){
-            btnConnect.click();
-        }
+  messageInput.onclick = () => {
+    messageInput.dispatchEvent(new InputEvent("input"));
+  };
+
+  messageInput.addEventListener("keydown", function (e) {
+    if (AutoCompleteSmileys.isOpen()) {
+      return;
     }
-
-    btnLogout.onclick = () => {
-        sock.emit("logout");
-        radLogScreen.checked = true;
-    }
-
-    messageInput.addEventListener('input',function(e){
-        if(messageInput.value.startsWith("/")){
-            AutoCompleteSmileys.setPause(true);
-        }else{
-            AutoCompleteSmileys.setPause(false);
-        }
-    })
-
-    messageInput.onclick = () => {
-        messageInput.dispatchEvent(new InputEvent('input'));
-    }
-
-    messageInput.addEventListener('keydown',function(e){
-        if(AutoCompleteSmileys.isOpen()){
-            return;
-        }
-        if (e.key === 'Enter' && messageInput.value.trim().length !== 0) {
-            e.preventDefault()
-            btnSend.click();
-        }else if(e.key === 'ArrowUp'){
-            e.preventDefault();
-            let command = history.prev();
-            if(command){
-                messageInput.value = command;
-                messageInput.dispatchEvent(new InputEvent('input'));
-            }
-        }else if(e.key === 'ArrowDown'){
-            e.preventDefault();
-            let command = history.next();
-            if(command){
-                messageInput.value = command;
-                messageInput.dispatchEvent(new InputEvent('input'));
-            }else{
-                messageInput.value = "";
-            }
-        }else if(e.which === 9){
-            e.preventDefault()
-            let match = messageInput.value.match(/^\/chifoumi @[^\s]+ :/)
-            if(match){
-                messageInput.dispatchEvent(new InputEvent('input'));
-                messageInput.value = `${match[0]}${autoCompleteChifoumi.next()}:`;
-            }
-        }
-    })
-
-    btnSend.onclick = () => {
-        let text = messageInput.value.trim();
+    if (e.key === "Enter" && messageInput.value.trim().length !== 0) {
+      e.preventDefault();
+      btnSend.click();
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault();
+      let command = history.prev();
+      if (command) {
+        messageInput.value = command;
+        messageInput.dispatchEvent(new InputEvent("input"));
+      }
+    } else if (e.key === "ArrowDown") {
+      e.preventDefault();
+      let command = history.next();
+      if (command) {
+        messageInput.value = command;
+        messageInput.dispatchEvent(new InputEvent("input"));
+      } else {
         messageInput.value = "";
+      }
+    } else if (e.which === 9) {
+      e.preventDefault();
+      let match = messageInput.value.match(/^\/chifoumi @[^\s]+ :/);
+      if (match) {
+        messageInput.dispatchEvent(new InputEvent("input"));
+        messageInput.value = `${match[0]}${autoCompleteChifoumi.next()}:`;
+      }
+    }
+  });
 
-        if(text !== history.commands[0]) {
-            history.commands.unshift(text);
+  btnSend.onclick = () => {
+    let text = messageInput.value.trim();
+    messageInput.value = "";
+
+    if (text !== history.commands[0]) {
+      history.commands.unshift(text);
+    }
+    history.index = -1;
+
+    if (text.startsWith("/")) {
+      if (/^\/chifoumi /.test(text)) {
+        let match = text.match(/^\/chifoumi ([^\s]+) ([^\s]+)$/);
+
+        if (match === null) {
+          addMessage(
+            "Nombre de paramètre incorrecte",
+            "[chifoumi]",
+            "chifoumi",
+            Date.now()
+          );
+          return;
         }
-        history.index = -1;
 
-        if(text.startsWith("/")){
-            if (/^\/chifoumi /.test(text)) {
-                let match = text.match(/^\/chifoumi ([^\s]+) ([^\s]+)$/);
-
-                if (match === null) {
-                    addMessage("Nombre de paramètre incorrecte", "[chifoumi]", "chifoumi", Date.now())
-                    return;
-                }
-
-                let opponentMatch = match[1].match(/^@([^\s]+$)/);
-                if (opponentMatch === null) {
-                    addMessage("Adversaire incorrecte ( @adversaire )", "[chifoumi]", "chifoumi", Date.now());
-                    return;
-                }
-
-                let opponent = opponentMatch[1];
-                if (opponent === pseudo) {
-                    addMessage("Vous ne pouvez pas vous défier vous même", "[chifoumi]", "chifoumi", Date.now());
-                    return;
-                }
-
-                let elementMatch = match[2].match(/^:(paper|rock|scissors|lizard|spock):$/);
-                if (elementMatch === null) {
-                    addMessage("Element incorrecte", "[chifoumi]", "chifoumi", Date.now());
-                    return;
-                }
-
-                let element = elementMatch[1];
-
-
-
-                sock.emit("chifoumi", {
-                    to: opponent,
-                    element: element,
-                })
-                return;
-
-            }else{
-                addMessage(`Invalid command - "${text.match(/\/\S*/)}"`,"[admin]",'system',Date.now());
-                return;
-            }
+        let opponentMatch = match[1].match(/^@([^\s]+$)/);
+        if (opponentMatch === null) {
+          addMessage(
+            "Adversaire incorrecte ( @adversaire )",
+            "[chifoumi]",
+            "chifoumi",
+            Date.now()
+          );
+          return;
         }
-        let to = text.match(/^@\S+/)
 
-        if (to) {
-            to = to[0].split("@")[1];
+        let opponent = opponentMatch[1];
+        if (opponent === pseudo) {
+          addMessage(
+            "Vous ne pouvez pas vous défier vous même",
+            "[chifoumi]",
+            "chifoumi",
+            Date.now()
+          );
+          return;
         }
-        sock.emit("message", {to: to, text: text})
 
+        let elementMatch = match[2].match(
+          /^:(paper|rock|scissors|lizard|spock):$/
+        );
+        if (elementMatch === null) {
+          addMessage(
+            "Element incorrecte",
+            "[chifoumi]",
+            "chifoumi",
+            Date.now()
+          );
+          return;
+        }
+
+        let element = elementMatch[1];
+
+        sock.emit("chifoumi", {
+          to: opponent,
+          element: element,
+        });
+        return;
+      } else {
+        addMessage(
+          `Invalid command - "${text.match(/\/\S*/)}"`,
+          "[admin]",
+          "system",
+          Date.now()
+        );
+        return;
+      }
+    }
+    let to = text.match(/^@\S+/);
+
+    if (to) {
+      to = to[0].split("@")[1];
+    }
+    sock.emit("message", { to: to, text: text });
+  };
+
+  // *****    SOCKECT     *****
+
+  sock.on("erreur-connexion", function (msg) {
+    alert(msg);
+    pseudo = null;
+  });
+
+  sock.on("bienvenue", function (clientsList) {
+    chatMain.innerHTML = "";
+    chatMain.appendChild(AutoCompleteSmileys.dom);
+    list = clientsList;
+    radContentScreen.checked = true;
+    document.getElementById("login").textContent = pseudo;
+    updateList();
+    if (pseudo.trim().toLowerCase() === "fred") {
+      guacamole();
+    }
+  });
+
+  sock.on("liste", function (clientsList) {
+    list = clientsList;
+    updateList();
+  });
+
+  sock.on("message", function ({ from, to, text, date }) {
+    // System message
+    if (from == null) {
+      addMessage(text, "[admin]", "system", date);
+      return;
     }
 
-    // *****    SOCKECT     *****
-
-    sock.on("erreur-connexion", function (msg) {
-        alert(msg);
-        pseudo = null;
-    })
-
-    sock.on("bienvenue", function (clientsList) {
-        chatMain.innerHTML = "";
-        chatMain.appendChild(AutoCompleteSmileys.dom);
-        list = clientsList;
-        radContentScreen.checked = true;
-        document.getElementById("login").textContent = pseudo;
-        updateList();
-        if(pseudo.trim().toLowerCase() === 'fred'){
-            guacamole()
-        }
-    })
-
-    sock.on("liste", function (clientsList) {
-        list = clientsList;
-        updateList()
-    })
-
-    sock.on("message", function ({from, to, text, date}) {
-        // System message
-        if (from == null) {
-            addMessage(text, '[admin]', 'system', date);
-            return;
-        }
-
-        // Broadcast message
-        if (to == null) {
-            addMessage(text, from, (from == pseudo ? 'moi' : ''), date);
-            return;
-        }
-
-        // Personal message
-        if (to === pseudo || from === pseudo) {
-            addMessage(text.split(/^@\S+/)[1], `${from} [privé${(from === pseudo) ? ` @${to}]` : ']'}`, 'mp', date);
-        }
-    })
-
-    sock.on("chifoumi", function ({text, date,buzz : toBuzz}) {
-        addMessage(text, '[chifoumi]', "chifoumi", date);
-        if(toBuzz){
-            buzz()
-        }
-
-    })
-
-    AutoCompleteSmileys.create(messageInput, elt('div', {id: "autocomplete"}))
-
-    // *****    FUNCTIONS     *****
-
-    /**
-     * Add a message into the chat content
-     * @param text The message content
-     * @param from
-     * @param type
-     * @param date
-     */
-    function addMessage(text, from, type, date) {
-        date = new Date(date);
-        text = parseSmileys(text);
-        let hours = ("0" + date.getHours()).slice(-2);
-        let minutes = ("0" + date.getMinutes()).slice(-2);
-        let secondes = ("0" + date.getSeconds()).slice(-2);
-        let msg = elt('p', {class: type}, `${hours}:${minutes}:${secondes} - ${from} : ${text}`);
-        chatMain.appendChild(msg);
+    // Broadcast message
+    if (to == null) {
+      addMessage(text, from, from == pseudo ? "moi" : "", date);
+      return;
     }
 
-    function buzz(){
-        content.classList.add('buzz');
-        setTimeout(()=>{
-            content.classList.remove('buzz');
-        },500);
+    // Personal message
+    if (to === pseudo || from === pseudo) {
+      addMessage(
+        text.split(/^@\S+/)[1],
+        `${from} [privé${from === pseudo ? ` @${to}]` : "]"}`,
+        "mp",
+        date
+      );
     }
+  });
 
-    function guacamole(){
-        document.body.classList.add('guacamole');
-        alert("Mode guacamole spécial M.Dadeau ! :)");
+  sock.on("chifoumi", function ({ text, date, buzz: toBuzz }) {
+    addMessage(text, "[chifoumi]", "chifoumi", date);
+    if (toBuzz) {
+      buzz();
     }
+  });
 
+  AutoCompleteSmileys.create(messageInput, elt("div", { id: "autocomplete" }));
 
-    /**
-     * Update the connected clients list
-     */
-    function updateList() {
-        asideClients.innerHTML = ""
-        list.forEach(client => {
-            asideClients.appendChild(elt(
-                'p',
-                {
-                    "data-score": client.score
-                },
-                (client.pseudo)
-            ))
-        })
-    }
+  // *****    FUNCTIONS     *****
 
-})
-;
-    
+  /**
+   * Add a message into the chat content
+   * @param text The message content
+   * @param from
+   * @param type
+   * @param date
+   */
+  function addMessage(text, from, type, date) {
+    date = new Date(date);
+    text = parseSmileys(text);
+    let hours = ("0" + date.getHours()).slice(-2);
+    let minutes = ("0" + date.getMinutes()).slice(-2);
+    let secondes = ("0" + date.getSeconds()).slice(-2);
+    let msg = elt(
+      "p",
+      { class: type },
+      `${hours}:${minutes}:${secondes} - ${from} : ${text}`
+    );
+    chatMain.appendChild(msg);
+  }
+
+  function buzz() {
+    content.classList.add("buzz");
+    setTimeout(() => {
+      content.classList.remove("buzz");
+    }, 500);
+  }
+
+  function guacamole() {
+    document.body.classList.add("guacamole");
+    alert("Mode guacamole spécial M.Dadeau ! :)");
+  }
+
+  /**
+   * Update the connected clients list
+   */
+  function updateList() {
+    asideClients.innerHTML = "";
+    list.forEach((client) => {
+      asideClients.appendChild(
+        elt(
+          "p",
+          {
+            "data-score": client.score,
+          },
+          client.pseudo
+        )
+      );
+    });
+  }
+});
--- test/chifoumi.test.js
+++ test/chifoumi.test.js
@@ -1,82 +1,70 @@
-const assert = require('assert');
-const chifoumi = require('../chifoumi');
-const {it, describe, beforeEach} = require("mocha");
+const assert = require("assert");
+const chifoumi = require("../chifoumi");
+const { it, describe, beforeEach } = require("mocha");
 
 const Duel = chifoumi.duel;
 
 describe("Chifoumi", function () {
+  let duel;
+  beforeEach("Create a new duel", function () {
+    duel = new Duel("François", "Fanny");
+  });
 
-    let duel;
-    beforeEach('Create a new duel', function () {
-        duel = new Duel("François", "Fanny");
-    })
+  describe("Result", function () {
+    it("No winner", function () {
+      assert(duel.result === null);
+    });
 
-    describe('Result', function () {
+    it("Good result", function () {
+      duel.play("François", "lizard");
+      duel.play("Fanny", "scissors");
 
-        it("No winner", function () {
-            assert(duel.result === null);
-        })
+      assert.deepStrictEqual(duel.result, {
+        winner: "Fanny",
+        message: ":scissors: decapitate :lizard:",
+      });
+    });
 
-        it("Good result", function () {
-            duel.play('François', 'lizard');
-            duel.play('Fanny', 'scissors');
+    it("Ex-aequo", function () {
+      duel.play("François", "paper");
+      duel.play("Fanny", "paper");
 
-            assert.deepStrictEqual(duel.result, {
-                winner: "Fanny",
-                message: ":scissors: decapitate :lizard:"
-            })
-        })
+      assert.deepStrictEqual(duel.result, {
+        winner: null,
+        message: ":paper: vs :paper:",
+      });
+    });
+  });
 
-        it("Ex-aequo", function () {
-            duel.play('François', 'paper');
-            duel.play('Fanny', 'paper');
+  describe("Get Player", function () {
+    it("Good name", function () {
+      assert.notStrictEqual(duel.getPlayer("François"), null);
+      assert.notStrictEqual(duel.getPlayer("Fanny"), null);
+    });
 
-            assert.deepStrictEqual(duel.result, {
-                winner: null,
-                message: ":paper: vs :paper:"
-            })
-        })
-    })
+    it("Bad name", function () {
+      assert.strictEqual(duel.getPlayer("Francois"), null);
+      assert.strictEqual(duel.getPlayer("Phanni"), null);
+    });
+  });
 
-    describe("Get Player", function () {
+  describe("Play", function () {
+    it("Bad element", function () {
+      assert.strictEqual(duel.play("François", "fire"), -4);
+    });
 
-        it("Good name", function () {
-            assert.notStrictEqual(duel.getPlayer('François'), null);
-            assert.notStrictEqual(duel.getPlayer('Fanny'), null);
-        })
+    it("Bad player", function () {
+      assert.strictEqual(duel.play("Fraçois", "rock"), -2);
+    });
 
-        it("Bad name", function () {
-            assert.strictEqual(duel.getPlayer("Francois"), null);
-            assert.strictEqual(duel.getPlayer("Phanni"), null);
-        })
-    })
+    it("Null parameters", function () {
+      assert.strictEqual(duel.play(null, "lizard"), -1);
+      assert.strictEqual(duel.play("François", null), -1);
+    });
 
-    describe("Play", function () {
-
-        it("Bad element", function () {
-            assert.strictEqual(duel.play("François", "fire"), -4)
-        })
-
-        it("Bad player", function () {
-            assert.strictEqual(duel.play('Fraçois', 'rock'), -2);
-        })
-
-        it("Null parameters", function () {
-            assert.strictEqual(duel.play(null, 'lizard'), -1);
-            assert.strictEqual(duel.play("François", null), -1);
-        })
-
-        it("Already played", function () {
-            assert.strictEqual(duel.play('Fanny', 'spock'), 0);
-            assert.strictEqual(duel.play('Fanny', 'paper'), -3);
-        })
-    })
-})
-
-
-
-
-
-
-
-
+    it("Already played", function () {
+      assert.strictEqual(duel.play("Fanny", "spock"), 0);
+      assert.strictEqual(duel.play("Fanny", "paper"), -3);
+    });
+  });
+});
